bfe0d1dbcd0aea6564aca48ba7220a4871520be3
betty29tesfaye <144184498+betty29tesfaye@users.noreply.github.com>
2024-03-05 20:18:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def add_background_image(image_file):
   
 def generate_test_cases(requirement):
     response = client.chat.completions.create(
-      model="gpt-3.5-turbo",
+      model="gpt-3.5-turbo",st.markdown(f'<span style="background-color:#DFF2FF;color:#0F52BA;font-family:book-antiqua;font-size:24px;">AI App For Text Summarizing</span>', unsafe_allow_html=True)
       messages=[
         {"role": "system", "content": "You are a helpful assistant capable of generating software test cases."},
         {"role": "user", "content": requirement}
@@ -37,5 +37,9 @@ def generate_test_cases(requirement):
     )
     return response.choices[0].message.content
   add_background_image('bgi.png') 
+st.markdown(f'<span style="background-color:#DFF2FF;color:#0F52BA;font-family:book-antiqua;font-size:24px;">AI App For Generating Test Case</span>', unsafe_allow_html=True)
+test_requirement = st.input("Hi there, Please enter the requirement of your test cases. Enter a statement similar to :The system shall allow users to edit the email body")
+if test_requirement:
+   st.write(generate_test_cases(test_requirement))
 
 
