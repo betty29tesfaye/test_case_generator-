808779b1acb08cb76cb5fe490da53243b4b3a1a1
betty29tesfaye <144184498+betty29tesfaye@users.noreply.github.com>
2024-03-15 10:50:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ chain = LLMChain(
 )
 add_background_image('bgi.png') 
 st.markdown(f'<span style="background-color:#DFF2FF;color:#0F52BA;font-family:book-antiqua;font-size:24px;">AI App For Generating Test Cases</span>', unsafe_allow_html=True)
-st.markdown(f'<p style="background-color:#b3cee5;color:#414C6B;">Hi there to get test cases, please enter a statement requirement similar to: The system shall allow users to edit the email body</p>', unsafe_allow_html=True) 
+st.markdown(f'<p style="background-color:#b3cee5;color:#414C6B;">Hi there to get test cases, please enter a statement requirement</p>', unsafe_allow_html=True) 
 system_requirement = st.text_input("") 
 if system_requirement:
    with st.spinner('Generating the test cases'):
